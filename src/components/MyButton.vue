d4d5608d357c20f3ad9cec7a807ece205e25402d
ll <572785890@qq.com>
2022-05-18 15:03:57 +0800

    first

---
+++ src/components/MyButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>hello {{count}}次</h2>
+    <button @click="count++">点我</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        count:0
+      }
+    },
+  }
+</script>
+
+<style>
+  h2{
+    color:red
+  }
+</style>
